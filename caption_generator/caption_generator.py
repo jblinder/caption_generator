d42fdbafc17df64a17d16bf744bfb32b14f569ee
Justin Blinder <justin.blinder@gmail.com>
2018-06-11 19:09:39 -0400

    Updated print statements and depedencies

--- caption_generator/caption_generator.py
+++ caption_generator/caption_generator.py
@@ -34,7 +34,7 @@ class CaptionGenerator():
         self.total_samples=0
         for text in caps:
             self.total_samples+=len(text.split())-1
-        print "Total samples : "+str(self.total_samples)
+        print("Total samples : "+str(self.total_samples))
         
         words = [txt.split() for txt in caps]
         unique = []
@@ -54,16 +54,16 @@ class CaptionGenerator():
             if(len(caption.split()) > max_len):
                 max_len = len(caption.split())
         self.max_cap_len = max_len
-        print "Vocabulary size: "+str(self.vocab_size)
-        print "Maximum caption length: "+str(self.max_cap_len)
-        print "Variables initialization done!"
+        print("Vocabulary size: "+str(self.vocab_size))
+        print("Maximum caption length: "+str(self.max_cap_len))
+        print("Variables initialization done!")
 
 
     def data_generator(self, batch_size = 32):
         partial_caps = []
         next_words = []
         images = []
-        print "Generating data..."
+        print("Generating data...")
         gen_count = 0
         df = pd.read_csv('Flickr8k_text/flickr_8k_train_dataset.txt', delimiter='\t')
         nb_samples = df.shape[0]
@@ -97,7 +97,7 @@ class CaptionGenerator():
                         partial_caps = sequence.pad_sequences(partial_caps, maxlen=self.max_cap_len, padding='post')
                         total_count = 0
                         gen_count+=1
-                        print "yielding count: "+str(gen_count)
+                        print("yielding count: "+str(gen_count))
                         yield [[images, partial_caps], next_words]
                         partial_caps = []
                         next_words = []
@@ -130,7 +130,7 @@ class CaptionGenerator():
         model.add(Dense(self.vocab_size))
         model.add(Activation('softmax'))
 
-        print "Model created!"
+        print("Model created!")
 
         if(ret_model==True):
             return model
